01981b5fa1bec8fa419b6d6b4208976ade521bb5
Вячеслав Поморцев <xaoasakura1991@gmail.com>
2023-09-23 17:46:49 +0300

    change second test

--- src/test/java/PatternsTest.java
+++ src/test/java/PatternsTest.java
@@ -14,41 +14,42 @@ public class PatternsTest {
         return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
     }
 
-        @Test
-        void shouldTest () {
-            open("http://localhost:9999/");
-            $("[data-test-id='city'] input").setValue("Хабаровск");
-            $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-            $("[data-test-id='date'] input").setValue(generateDate(3, "dd.MM.yyyy"));
-            $("[data-test-id='name'] input").setValue("Петров Петр");
-            $("[data-test-id='phone'] input").setValue("+79990009900");
-            $("[data-test-id='agreement']").click();
-            $(".button").click();
-            $(".notification__title")
-                    .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                    .shouldBe(Condition.text("Успешно!"));
+    @Test
+    void shouldTest() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Хабаровск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").setValue(generateDate(3, "dd.MM.yyyy"));
+        $("[data-test-id='name'] input").setValue("Петров Петр");
+        $("[data-test-id='phone'] input").setValue("+79990009900");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $(".notification__title")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldBe(Condition.text("Успешно!"));
 
-        }
-
-        @Test
-        void shouldTestSecondTime () {
-            open("http://localhost:9999/");
-            $("[data-test-id='city'] input").setValue("Хабаровск");
-            $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-            String planningDate = generateDate(3, "dd.MM.yyyy");
-            $("[data-test-id='date'] input").setValue(generateDate(3, "dd.MM.yyyy"));
-            $("[data-test-id='name'] input").setValue("Петров Петр");
-            $("[data-test-id='phone'] input").setValue("+79990009900");
-            $("[data-test-id='agreement']").click();
-            $(".button").click();
-            $("[data-test-id='replan-notification']")
-                   .shouldBe(Condition.text("Необходимо подтверждение"));
-            $("button .button__content").click();
-            $(".notification__content")
-                    .shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate));
-
-
-        }
     }
+
+    @Test
+    void shouldTestSecondTime() {
+        open("http://localhost:9999/");
+        $("[data-test-id='city'] input").setValue("Хабаровск");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        String planningDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").setValue(generateDate(4, "dd.MM.yyyy"));
+        $("[data-test-id='name'] input").setValue("Петров Петр");
+        $("[data-test-id='phone'] input").setValue("+79990009900");
+        $("[data-test-id='agreement']").click();
+        $(".button").click();
+        $("[data-test-id='replan-notification'] span.button__text").click();
+        //$("[data-test-id='replan-notification']")
+                //.shouldBe(Condition.text("Необходимо подтверждение"));
+        //$("button .button__content").click();
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate));
+
+
+    }
+}
 
 
