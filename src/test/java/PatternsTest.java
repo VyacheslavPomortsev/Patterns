88bf7e74f8981451cb867c4a67cd8734947df013
Вячеслав Поморцев <xaoasakura1991@gmail.com>
2023-09-24 12:27:58 +0300

    only one test

--- src/test/java/PatternsTest.java
+++ src/test/java/PatternsTest.java
@@ -27,27 +27,13 @@ public class PatternsTest {
         $(".notification__title")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldBe(Condition.text("Успешно!"));
-
-    }
-
-    @Test
-    void shouldTestSecondTime() {
-        open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Хабаровск");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
         String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").setValue(generateDate(4, "dd.MM.yyyy"));
-        $("[data-test-id='name'] input").setValue("Петров Петр");
-        $("[data-test-id='phone'] input").setValue("+79990009900");
-        $("[data-test-id='agreement']").click();
         $(".button").click();
         $("[data-test-id='replan-notification'] span.button__text").click();
-        //$("[data-test-id='replan-notification']")
-                //.shouldBe(Condition.text("Необходимо подтверждение"));
-        //$("button .button__content").click();
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно запланирована на " + planningDate));
-
 
     }
 }
