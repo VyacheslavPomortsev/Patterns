a785f2b472d837c85e56df2517992a40f84176bc
Вячеслав Поморцев <xaoasakura1991@gmail.com>
2023-09-28 12:39:05 +0300

    add DataGenerator

--- src/test/java/PatternsTest.java
+++ src/test/java/PatternsTest.java
@@ -10,26 +10,28 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class PatternsTest {
-    private String generateDate(int addDays, String pattern) {
-        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
-    }
 
     @Test
     void shouldTest() {
         open("http://localhost:9999/");
-        $("[data-test-id='city'] input").setValue("Хабаровск");
+        DataGenerator.UserInfo validUser = DataGenerator.Registration.generateUser("ru");
+        int firstMeetingDay = 4;
+        String firstDay = DataGenerator.generateDate(firstMeetingDay);
+        int secondMeetingDay = 7;
+        String secondDay = DataGenerator.generateDate(secondMeetingDay);
+        $("[data-test-id='city'] input").setValue(validUser.getCity());
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(generateDate(3, "dd.MM.yyyy"));
-        $("[data-test-id='name'] input").setValue("Петров Петр");
-        $("[data-test-id='phone'] input").setValue("+79990009900");
+        $("[data-test-id='date'] input").setValue(firstDay);
+        $("[data-test-id='name'] input").setValue(validUser.getName());
+        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
         $("[data-test-id='agreement']").click();
         $(".button").click();
         $(".notification__title")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
                 .shouldBe(Condition.text("Успешно!"));
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        String planningDate = generateDate(4, "dd.MM.yyyy");
-        $("[data-test-id='date'] input").setValue(generateDate(4, "dd.MM.yyyy"));
+        String planningDate = secondDay;
+        $("[data-test-id='date'] input").setValue(secondDay);
         $(".button").click();
         $("[data-test-id='replan-notification'] span.button__text").click();
         $(".notification__content")
